6ec969b14c7f0c38f7f14606dc841d0069041a5b
GregOSF <gregory.onza@gmail.com>
2015-07-20 11:21:22 -0700

    Create city and neighborhood models

--- models/nabe.js
+++ models/nabe.js
@@ -6,15 +6,45 @@ var mongoose = require('mongoose'),
 
 
 // create schema for cities
-var CitySchema = new Schema({
-	name: {
+var citySchema = new Schema({
+	cityName: {
 		type: 'string',
 		default: ""
-		
+	},
+	cityNabes: {
+		type: 'string',
+		default: ""
+	},
+	users: {
+		type: 'string',
+		default: ""
 	}
-})
+});
 
+// create schema for neighborhoods
+var nabeSchema = new Schema ({
+	nabeName: {
+		type: 'string',
+		default: ""
+	},
+	nabeDesc: {
+		type: 'string',
+		default: ""
+	},
+	nabeTags: {
+		type: 'string',
+		default: ""
+	},
+	cityName{
+		type: 'string',
+		default: ""
+	},
+});
 
+//create models for city and nabe
+var City = mongoose.model('City', citySchema);
+var Nabe = mongoose.model('Nabe', nabe);
 
-
-var NabeSchema
+// export models
+module.exports.City = City;
+module.exports.Nabe = Nabe;
