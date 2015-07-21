b9e61ef72abe8c29342b39cb5ca4d6545f7dc595
GregOSF <gregory.onza@gmail.com>
2015-07-21 14:06:09 -0700

    rename file

---
+++ models/nabes.js
@@ -0,0 +1,42 @@
+// require mongoose
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+
+
+
+// create schema for cities
+var citySchema = new Schema({
+	cityName: {
+		type: 'string',
+		default: ""
+	},
+	cityNabes: {
+		type: 'string',
+		default: ""
+	}
+});
+
+// create schema for neighborhoods
+var nabesSchema = new Schema ({
+	nabesName: {
+		type: 'string',
+		default: ""
+	},
+	nabesTags: {
+		type: 'string',
+		default: ""
+	},
+	cityName: {
+		type: 'string',
+		default: ""
+	}
+});
+
+//create models for city and nabe
+var City = mongoose.model('City', citySchema);
+var Nabes = mongoose.model('Nabes', nabesSchema);
+
+// export models
+module.exports.City = City;
+module.exports.Nabes = Nabes;
