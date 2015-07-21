51f149420a22d5d51fb5aaaf0d71ed95ad4547d6
GregOSF <gregory.onza@gmail.com>
2015-07-21 15:20:26 -0700

    delete extra space

--- seed.js
+++ seed.js
@@ -1,1 +1,13 @@
-var models = require("./models")
+var mongoose = require('mongoose');
+var dbNabes = require("./models/nabes");
+
+mongoose.connect('mongodb://localhost/nabes1');
+
+db.nabes.insert({
+	nabesName: "Castro", 
+	nabesTags: "Fabulous",
+	cityName: "San Francisco"
+});
+
+
+// module.export.nabes = Nabes;
