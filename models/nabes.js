b41834f53b089495cd122bd05e66d24200792b47
GregOSF <gregory.onza@gmail.com>
2015-07-23 14:01:02 -0700

    Comment lines

--- models/nabes.js
+++ models/nabes.js
@@ -1,21 +1,9 @@
 // require mongoose
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
+	City = require('./cities');
+	NabesNY = require('./nabesny')
 
-
-
-
-// create schema for cities
-var citySchema = new Schema({
-	cityName: {
-		type: 'string',
-		default: ""
-	},
-	cityNabes: {
-		type: 'string',
-		default: ""
-	}
-});
 
 // create schema for sf neighborhoods
 var NabesSFSchema = new Schema ({
@@ -29,24 +17,41 @@ var NabesSFSchema = new Schema ({
 	}	
 });
 
+
+
+// create schema for cities
+// var citySchema = new Schema({
+// 	cityName: {
+// 		type: 'string',
+// 		default: ""
+// 	},
+// 	cityNabes: [
+// 		{
+// 			type: Schema.Types.ObjectId,
+// 			ref: 'NabesSF'
+// 		}]
+// });
+
+
+
 // create schema for ny neighborhoods
-var NabesNYSchema = new Schema ({
-	nabesName: {
-		type: 'string',
-		default: ""
-	},
-	nabesTags: {
-		type: 'string',
-		default: ""
-	}	
-});
+// var NabesNYSchema = new Schema ({
+// 	nabesName: {
+// 		type: 'string',
+// 		default: ""
+// 	},
+// 	nabesTags: {
+// 		type: 'string',
+// 		default: ""
+// 	}	
+// });
 
 //create models for city and nabe
-var City = mongoose.model('City', citySchema);
+// var City = mongoose.model('City', citySchema);
 var NabesSF = mongoose.model('nabessfs', NabesSFSchema);
 // var NabesNY = mongoose.model('nabesNY', NabesNYSchema);
 
 // export models
-module.exports = City;
+// module.exports = City;
 module.exports = NabesSF;
 // module.exports = NabesNY;
