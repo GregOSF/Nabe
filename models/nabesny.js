4bbc387c88de19774765fa91b79230b4d3cd902e
GregOSF <gregory.onza@gmail.com>
2015-07-23 14:00:29 -0700

    Create file; add nynabes schema

---
+++ models/nabesny.js
@@ -0,0 +1,58 @@
+// require mongoose
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	NabesSF = require('./nabes'),
+	City = require('./cities');
+
+
+
+// create schema for sf neighborhoods
+// var NabesSFSchema = new Schema ({
+// 	nabesName: {
+// 		type: 'string',
+// 		default: ""
+// 	},
+// 	nabesTags: {
+// 		type: 'string',
+// 		default: ""
+// 	}	
+// });
+
+
+
+// // create schema for cities
+// var citySchema = new Schema({
+// 	cityName: {
+// 		type: 'string',
+// 		default: ""
+// 	},
+// 	cityNabes: [
+// 		{
+// 			type: Schema.Types.ObjectId,
+// 			ref: 'NabesSF'
+// 		}]
+// });
+
+
+
+// create schema for ny neighborhoods
+var NabesNYSchema = new Schema ({
+	nabesName: {
+		type: 'string',
+		default: ""
+	},
+	nabesTags: {
+		type: 'string',
+		default: ""
+	}	
+});
+
+//create models for city and nabe
+// var City = mongoose.model('City', citySchema);
+// var NabesSF = mongoose.model('nabessfs', NabesSFSchema);
+var NabesNY = mongoose.model('nabesNY', NabesNYSchema);
+
+// export models
+// module.exports = City;
+// module.exports = NabesSF;
+module.exports = NabesNY;
