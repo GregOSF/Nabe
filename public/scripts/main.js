03220f3812ea7c4af6ac28a20690994daec63da1
GregOSF <gregory.onza@gmail.com>
2015-07-24 03:29:38 -0700

    Clean up code; modify events

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,177 +1,5 @@
 $(function() {
 
-  var $nabeLinkTemplate = _.template($('#nabe-link-template').html());
-
-  $.get('api/sfnabes', function(data){
-    _.each(data, function(nabe) {
-      nabe.nabeClass = "sf-nabe"
-      console.log(nabe)
-      $('#sf-list').append($nabeLinkTemplate(nabe));
-    })
-    $('.sf-nabe').click(function(e) {
-      var nabe = { _id: $(this).attr('id') }
-      $('.nyc-nabe').hide();
-
-      $.post('/nyc-search', nabe, function(data) {
-        // data == array of nabe ids
-        _.each(data, function(el) {
-          $("#" + el).show();
-        });
-      });
-    });
-  });
-
-  $.get('api/nyNabes', function(data){
-    console.log(data);
-    _.each(data, function(nabe) {
-      nabe.nabeClass = "nyc-nabe"
-      $('#nyc-list').append($nabeLinkTemplate(nabe));
-    })
-    $('.nyc-nabe').click(function(e) {
-      var nabe = { _id: $(this).attr('id') }
-      $('.sf-nabe').hide();
-      $.post('/sf-search', nabe, function(data) {
-        // data == array of nabe ids
-        _.each(data, function(el) {
-          $("#" + el).show();
-        // })
-        });
-      });
-    });
-  });
-
-  $('#clear').click(function(e) {
-    // show all nabes
-  });
-
-
-// BEGIN DROPDOWN BEGIN DROPDOWNBEGIN DROPDOWNBEGIN DROPDOWNBEGIN DROPDOWN
-
-
-  // var nabeController = {
-
-  //   // City List Template
-
-  //   templateCity: _.template($('#cityListTemplate').html()),
-
-  //   // Nabe Dropdown Template
-  //   templateDrop: _.template($('#nabeListTemplate').html()),
-
-  //   // NabeSF 
-  //   templateSf: _.template($('#nabeSfTemp').html()),
-
-  //   // Nabe
-    
-  //   // Render City Dropdown
-
-  //   renderCityDrop: function(cityObj) {
-  //     var $citiesHtml = $(nabeController.templateCity(cityObj));
-  //     $('#citySelect').append($citiesHtml);
-  //   },
-
-  //   // Drop Down City
-  //   cityDrops: function() {
-  //     $.get ('/api/cities', function(data) {
-  //       var allCities = data;
-  //       _.each(allCities, function(cit) {
-  //         nabeController.renderCityDrop(cit);
-  //       });
-  //       console.log(allCities);
-  //     })
-  //   },
-
-  //   // Render Neighborhood Dropdown
-
-  //   renderNabeDrop: function(nabeObj) {
-  //     var $nabesHtml = $(nabeController.templateDrop(nabeObj));
-  //     $('#nabeSelect').append($nabesHtml);
-  //   },
-
-  //   // Render SF (Left) Panel
-
-  //   renderSfPanel: function(nabe1) {
-  //     var $nabesHtml = $(nabeController.templateSf(nabe1));
-  //     $('#nabeSfPanel').append($nabesHtml);
-  //   },
-
-  //   // Drop Down SF
-  //   nabeDrops: function() {
-  //     $.get ('/api/sfNabes', function(data) {
-  //       var allNabes = data;
-
-  //       _.each(allNabes, function(nabe) {
-  //         nabeController.renderNabeDrop(nabe);
-  //         // console.log(allNabes);
-          
-  //       // });
-  //       // postController.addEventHandlers();
-  //       });
-  //         nabeController.nabePickClick();
-  //     });
-  //   },
-
-  //   // Show Neighborhood Clicked from Dropdown
-  //   nabePickDrop: function (nabesName) {
-  //       $.get ('/api/sfNabes/' + nabesName, function(data) {
-  //         var oneNabe = data;
-  //         console.log(oneNabe);
-  //         // _.each(oneNabe, function(nabe2) {
-  //           nabeController.renderSfPanel(oneNabe);
-  //       });
-
-  //   },  
-
-  //   // Event handler for clicking nabe dropdown
-  //   nabePickClick: function () {
-
-  //     // $('#nabeClick').click(function(e) {
-  //     //     alert('alerted' + poop);
-  //     //     e.preventDefault();// prevent the default anchor functionality
-  //     // });
-
-
-  //     $('#nabeClick').on('click', function(event) {
-  //       event.preventDefault();
-  //       console.log("poop");
-  //       var nabeId = $(this).closest('#nabeListItems').attr('data-id');
-  //       nabeController.nabePickDrop(nabeId);
-  //       console.log(nabeId);
-  //     });
-  //   }
-
-
-
-
-  //       // // find the posts's id (stored in HTML as `data-id`)
-  //       // var postId = $(this).closest('.blogPost').attr('data-id');
-  //       // // udpate the post with form data
-  //       // var updatedUser = $(this).find('.updated-user').val();
-  //       // var updatedLoc = $(this).find('.updated-loc').val();
-  //       // var updatedDesc = $(this).find('.updated-post').val();
-  //       // postController.update(postId, updatedUser, updatedLoc, updatedDesc);
-  //       // })
-
-
-  //   // nabePanelOne: function() {
-  //   //   $.get ('/api/sfNabes', function(data) {
-  //   //     var nabetags = data;
-
-  //   //     _.each(nabetags, function(nabe2) {
-  //   //       nabeController.renderSfPanel(nabe2);
-  //   //     // });
-  //   //     // postController.addEventHandlers();
-  //   //     });
-  //   //   });
-  //   // },
-
-
-  // };
-
-  // nabeController.cityDrops();
-  // nabeController.nabeDrops();
-
-    // nabeController.nabePanelOne();
-
   var navController = {
 
     // compile underscore template for nav links
@@ -192,196 +20,63 @@ $(function() {
     }
   };
 
-  // nabeController.all();
   navController.showCurrentUser();
+
+  // template for neighborhood lists
+  var $nabeLinkTemplate = _.template($('#nabe-link-template').html());
+
+  // AJAX call to get list of SF Nabes and render on page
+    // Click event for AJAX call to search NyNabes DB for matching values
+  $.get('api/sfnabes', function(data){
+    _.each(data, function(nabe) {
+      nabe.nabeClass = "sf-nabe"
+      console.log(nabe)
+      $('#sf-list').append($nabeLinkTemplate(nabe));
+    })
+    $('.sf-nabe').click(function(e) {
+      var nabe = { _id: $(this).attr('id') }
+      $('.nyc-nabe').hide();
+      $.post('/nyc-search', nabe, function(data) {
+        _.each(data, function(el) {
+          $("#" + el).show();
+        });
+      });
+    });
+  });
+
+  // AJAX call to get list of NY Nabes and render on page
+    // Click event for AJAX call to search SfNabes DB for matching values
+  $.get('api/nyNabes', function(data){
+    console.log(data);
+    _.each(data, function(nabe) {
+      nabe.nabeClass = "nyc-nabe"
+      $('#nyc-list').append($nabeLinkTemplate(nabe));
+    })
+    $('.nyc-nabe').click(function(e) {
+      var nabe = { _id: $(this).attr('id') }
+      $('.sf-nabe').hide();
+      $.post('/sf-search', nabe, function(data) {
+        _.each(data, function(el) {
+          $("#" + el).show();
+        // })
+        });
+      });
+    });
+  });
+
+  $('#clear').click(function(e) {    
+  });
+
+  // Initially hide nabe lists until click event
+  $('#sf-list').css("display", "none");
+  $('#sfHead').on("click", function () {
+    $('#sf-list').show();
+    $('#nyc-list').show();
+  });
+
+  $('#nyc-list').css("display", "none");
+  $('#nyhead').on("click", function () {
+    $('#nyc-list').show();
+    $('#sf-list').show()
+  });
 });
-
-// END DROPDOWN END DROPDOWN END DROPDOWNEND DROPDOWNEND DROPDOWNEND DROPDOWN
-
-    // Post new data
-    // create: function(newUser, newPlace, newDesc) {
-    //   var newPostData = {user: newUser, place: newPlace, post: newDesc};
-    //       $.post ('/blogposts', newPostData, function(data) {
-    //         var newPost = data;
-    //         postController.render(newPost)
-    //       });
-    // },
-
-    // Update existing posts
-    // update: function (postId, updatedUser, updatedLoc, updatedDesc) {
-    //   $.ajax( {
-    //     type: 'PUT',
-    //     url: '/blogposts/' + postId,
-    //     data: {
-    //       user: updatedUser,
-    //       place: updatedLoc,
-    //       post: updatedDesc
-    //     },
-    //     success: function (data) {
-    //       var updatedPost = data;
-    //       var $blogHtml = $(postController.template(updatedPost));
-    //       $('#blog-' + postId).replaceWith($blogHtml);
-    //     }
-    //   });
-    // },
-
-    // delete: function(postId) {
-    //   // send DELETE request to server to delete phrase
-    //   $.ajax({
-    //     type: 'DELETE',
-    //     url: '/blogposts/' + postId,
-    //     success: function(data) {   
-    //       // remove deleted phrase from view
-    //       $('#blog-' + postId).remove();
-    //     }
-    //   });
-    // },
-
-    // Event Handlers for Updating/Deleting
-
-    // addEventHandlers: function() {
-    //   $('#new-Posts')
-
-    //     .on('submit', '.update-post', function(event) {
-    //     event.preventDefault();
-    //     console.log("poop");
-    //     // find the posts's id (stored in HTML as `data-id`)
-    //     var postId = $(this).closest('.blogPost').attr('data-id');
-    //     // udpate the post with form data
-    //     var updatedUser = $(this).find('.updated-user').val();
-    //     var updatedLoc = $(this).find('.updated-loc').val();
-    //     var updatedDesc = $(this).find('.updated-post').val();
-    //     postController.update(postId, updatedUser, updatedLoc, updatedDesc);
-    //     })
-                      
-    //     // for delete: click event on `.removeButton` button
-    //     .on('click', '.removeButton', function(event) {
-    //     event.preventDefault();
-    //     // find the post's id
-    //     var postId = $(this).closest('.blogPost').attr('data-id');
-    //     // delete the posts
-    //     postController.delete(postId);
-    //   });
-    // },
-
-  //   setupView: function() {
-  //     // Append existing blog posts
-  //     nabeController.all();
-  //     // Add event handler to new blogpost
-  //     $('#new-Entry').on('submit', function(event) {
-  //       event.preventDefault();
-  //       // create new blog post
-  //       var $newUser = $('#user-name').val();
-  //       var $newPlace = $('#user-place').val();
-  //       var $newDesc = $('#user-post').val();
-  //       postController.create($newUser, $newPlace, $newDesc);
-  //       // form reset
-  //       $(this)[0].reset();
-  //       $('#user-name').focus();
-  //     });
-  //   }
-  // };
-
-  // nabeController.setupView();
-
-
-  /*function NewPost(user, loc, post) {
-    this.user = user;
-    this.loc = loc;
-    this.post = post;
-  }*/
-
-  
-
-  /*NewPost.all = [
-        new NewPost('Jack', 'Patagonia', 'Cupcake ipsum dolor sit. Amet I love liquorice jujubes pudding croissant I love pudding. Apple pie macaroon toffee jujubes pie tart cookie applicake caramels. Halvah macaroon I love lollipop. Wypas I love pudding brownie cheesecake tart jelly-o. Bear claw cookie chocolate bar jujubes toffee.'),
-        new NewPost('Alice', 'Himalayas', 'Cupcake ipsum dolor sit. Amet I love liquorice jujubes pudding croissant I love pudding. Apple pie macaroon toffee jujubes pie tart cookie applicake caramels. Halvah macaroon I love lollipop. Wypas I love pudding brownie cheesecake tart jelly-o. Bear claw cookie chocolate bar jujubes toffee.'),
-        new NewPost('Sarah', 'Grand Canyon', 'Cupcake ipsum dolor sit. Amet I love liquorice jujubes pudding croissant I love pudding. Apple pie macaroon toffee jujubes pie tart cookie applicake caramels. Halvah macaroon I love lollipop. Wypas I love pudding brownie cheesecake tart jelly-o. Bear claw cookie chocolate bar jujubes toffee.')
-  ];*/
-
- /* NewPost.prototype.save = function() {
-    NewPost.all.push(this);
-    console.log(NewPost.all);
-  };
-
-  NewPost.prototype.render = function() {
-    var $newPost = $(newPostTemplate(this));
-    this.index = NewPost.all.indexOf(this);
-    $newPost.attr('data-index', this.index);
-
-    $newPosts.prepend($newPost);
-  };
-
-  var $newEntry = $('#new-Entry');  
-  var $newPosts = $('#new-Posts');
-  var newPostTemplate = _.template($('#post-template').html());
-
-  
-  _.each(NewPost.all, function (NewPost, index) {
-    NewPost.render();
-  });
-
-  
-  $newEntry.on('submit', function(event) {
-    event.preventDefault();
-
-  
-  var $userName = $('#user-name').val();
-  var $userLoc = $('#user-location').val();
-  var $userPost = $('#user-post').val();
-  var newBlog = new NewPost($userName, $userLoc, $userPost);
-  var $postCont = $('#newPostContainer')
-
-  
-  newBlog.save();
-
-
-  newBlog.render();
-
-
-  $newEntry[0].reset();
-  $('#user-name').focus();
-  });
-*/
-  
-/*  $newPosts.on('click', '.blogPost', function() {
-    $(this).toggleClass('removePost');
-  });*/
-
-/*  var $blogPost = $('.blogPost')
-  var $removeButton = $('.removeButton')*/
-
-  /*$blogPost.on("click", function (event) {
-    event.preventDefault();
-    var $blogPosts = $(this).closest(".blogPost");
-    var index = $blogPost.attr('data-index');
-    $(this).fadeOut("slow");*/
-/*
-    $(document).on("click", '.blogPost', function (event) {
-      event.preventDefault();
-      var $blogPosts = $(this).closest(".blogPost");
-      var index = $blogPost.attr('data-index');
-      $(this).fadeOut("slow");*/
-
-/*  $('#new-Posts').on("click", function(event) {
-  event.preventDefault();
-  $(this).fadeOut("slow");
-  });
-*/
-  
-/*  NewPost.all.splice(index, 1);
-  console.log(NewPost.all);*/
-
-
-/*  $blogPost.remove();*/
-
-
-/*  $('.blogPost').each(function(index) {
-    $(this).attr('data-index', index);
- 
-  });*/
-
-
-
-
-
