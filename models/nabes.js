d626b2263ddd8d4a3fa6194c24f0f1bd95b22719
GregOSF <gregory.onza@gmail.com>
2015-07-23 18:10:45 -0700

    Clean up code

--- models/nabes.js
+++ models/nabes.js
@@ -17,41 +17,8 @@ var NabesSFSchema = new Schema ({
 	}	
 });
 
-
-
-// create schema for cities
-// var citySchema = new Schema({
-// 	cityName: {
-// 		type: 'string',
-// 		default: ""
-// 	},
-// 	cityNabes: [
-// 		{
-// 			type: Schema.Types.ObjectId,
-// 			ref: 'NabesSF'
-// 		}]
-// });
-
-
-
-// create schema for ny neighborhoods
-// var NabesNYSchema = new Schema ({
-// 	nabesName: {
-// 		type: 'string',
-// 		default: ""
-// 	},
-// 	nabesTags: {
-// 		type: 'string',
-// 		default: ""
-// 	}	
-// });
-
-//create models for city and nabe
-// var City = mongoose.model('City', citySchema);
+//create models for NabesSF
 var NabesSF = mongoose.model('nabessfs', NabesSFSchema);
-// var NabesNY = mongoose.model('nabesNY', NabesNYSchema);
 
 // export models
-// module.exports = City;
 module.exports = NabesSF;
-// module.exports = NabesNY;
